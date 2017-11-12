89bdecfc3ae0722533b133e0475f1ec587ab4487
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2017-11-11 23:00:52 -0800

    Implemented a better topKSort()

--- project3/src/main/java/misc/Searcher.java
+++ project3/src/main/java/misc/Searcher.java
@@ -21,31 +21,30 @@ public class Searcher {
      * @throws IllegalArgumentException  if k < 0
      */
     public static <T extends Comparable<T>> IList<T> topKSort(int k, IList<T> input) {
-        // Implementation notes:
-        //
-        // - This static method is a _generic method_. A generic method is similar to
-        //   the generic methods we covered in class, except that the generic parameter
-        //   is used only within this method.
-        //
-        //   You can implement a generic method in basically the same way you implement
-        //   generic classes: just use the 'T' generic type as if it were a regular type.
-        //
-        // - You should implement this method by using your ArrayHeap for the sake of
-        //   efficiency.
     		if (k < 0) {
 			throw new IllegalArgumentException();
 		} else if (input.isEmpty()) {
 			return new DoubleLinkedList<T>();
 		}
 		IPriorityQueue<T> heap = new ArrayHeap<T>();
-		for (int i = 0; i < input.size(); i++) {
+		// insert the first k elements into the heap
+		for (int i = 0; i < k; i++) {
 			heap.insert(input.get(i));
 		}
-		IList<T> result = new DoubleLinkedList<T>();
-		int remaining = heap.size() - k;
-		for (int i = 0; i < remaining; i++) {
-			heap.removeMin();
+		// the root of the heap is the kth largest element (smallest element)
+		for (int i = k; i < input.size(); i++) {
+			T element = input.get(i);
+			T heapMin = heap.peekMin();
+			// check if the new element is larger
+			// if it is, it should be the new kth largest element
+			if (element.compareTo(heapMin) > 0) {
+				heap.removeMin();
+				// replace the min with a 'larger' element
+				heap.insert(element);
+			}
 		}
+		// by the end of this loop, the heap only has the top k elements
+		IList<T> result = new DoubleLinkedList<T>();
 		for (int i = 0; i < k; i++) {
 			result.add(heap.removeMin());
 		}
